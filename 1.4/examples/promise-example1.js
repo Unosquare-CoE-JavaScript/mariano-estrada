3e3a4486f6de7304f6daed2c599e3c979c403147
MarianoUnosquare17 <mariano.estrada@unosquare.com>
2022-07-04 13:52:01 -0500

    added notes and exercises up until async/await

---
+++ 1.4/examples/promise-example1.js
@@ -0,0 +1,24 @@
+const swapi = function(num){
+    let url = 'https://swapi.dev/api/people/'
+    fetch(url + num + '/')
+    .then(function(data){
+        return data.json()
+    })
+    .then(function(obj){
+        console.log(obj)
+    })
+}
+
+swapi(1)
+
+console.log('other actions')
+
+//using arrow functions
+let arrowFetch = function(num){
+    const url = 'https://swapi.dev/api/people/'
+    fetch(url + num + '/')
+    .then( result => result.json())
+    .then( resultJson  => console.log(resultJson))
+}
+
+arrowFetch(2)
---
+++ 1.4/examples/promise-example2.js
@@ -0,0 +1,16 @@
+
+const todo = {
+    completed: false,
+    userId: 1,
+    title: 'learn promises'
+}
+
+fetch('https://jsonplaceholder.typicode.com/todos/', {
+    method: 'POST',
+    headers:{
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(todo) 
+})
+.then(res => res.json())
+.then(obj => console.log(obj))
