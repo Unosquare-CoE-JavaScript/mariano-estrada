b0b0ef944d6762145746a63073186e6d79d9891f
MarianoUnosquare17 <mariano.estrada@unosquare.com>
2022-07-07 20:41:10 -0500

    finished exercise 1

--- 1.5/exercises/react-exercise-1/src/components/Expenses/Expenses.js
+++ 1.5/exercises/react-exercise-1/src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -24,6 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses = {filteredExpenses}/>
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
