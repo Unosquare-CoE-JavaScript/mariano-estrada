9bd115a82f07598c51a78883cf26ad4aa3b2cca0
MarianoUnosquare17 <mariano.estrada@unosquare.com>
2022-07-18 16:57:28 -0500

    added food app exercise

--- 1.5/exercises/react-food-app/src/components/Layout/Header.js
+++ 1.5/exercises/react-food-app/src/components/Layout/Header.js
@@ -8,8 +8,7 @@ const Header = (props) => {
     <>
         <header className={classes.header}>
             <h1>React Food</h1>
-            <button>Cart</button>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick ={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}>
             <img src={meals} alt='...'/>
